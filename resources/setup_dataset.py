21ef91fb1dcc59f0aa6ac402923011c2c026164e
tristan-ranff <49940146+tristan-ranff@users.noreply.github.com>
2019-12-20 17:03:35 +0100

    nothing yet. config parameters now separate files.

---
+++ resources/setup_dataset.py
@@ -0,0 +1,37 @@
+
+
+def combine_ursgal_csv_files(
+        path_dict,
+        output_file
+):
+    """
+    Combine separate ursgal search output csv files and return a dataframe. Also output as new csv file (optional).
+    Takes a dictionary of input csv files and their respective engine and name of score column.
+
+    Args:
+        path_dict (str): path to ursgal path dict as .json
+        output_file (str): path to save new data frame to, do not save if None (default)
+    Returns:
+        input_df (pd.DataFrame): combined dataframes
+
+    """
+
+def make_dataset(
+        file_name,
+        input_df,
+        engines,
+        only_top,
+        balance_dataset,
+        combine_engines,
+        keep_ursgal,
+):
+    """
+
+    file_name:
+    input_df: x
+    engines:
+    only_top:
+    balance_dataset:
+    combine_engines: x
+    keep_ursgal: x
+    """
