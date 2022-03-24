d33d5d1fcd67d5dfefd2ba0c14db2f602ffc7166
tristan-ranff <tristan.raff@me.com>
2022-03-24 17:00:05 +0100

    improved setup and cleaned up analysis scripts

--- peptide_forest_3.py
+++ peptide_forest_3.py
@@ -6,15 +6,13 @@ if __name__ == "__main__":
     mp.freeze_support()
     mp.set_start_method("fork", force=True)
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", help="initial engine (optional)")
-    parser.add_argument("-u", help="ursgal path dict json")
+    parser.add_argument("-c", help="config path json")
     parser.add_argument("-o", help="output file")
     args = parser.parse_args()
 
     pf = peptide_forest.PeptideForest(
-        ursgal_path_dict=args.u,
+        config_path=args.c,
         output=args.o,
-        initial_engine=args.i,
     )
     pf.prep_ursgal_csvs()
     pf.calc_features()
