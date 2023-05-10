2dc5469e8413f35d2786cc9d9ae70bf10452746d
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-05-10 11:07:12 +0200

    adding hyperparameters for XGBoost and AdaBoost

--- peptide_forest/knowledge_base.py
+++ peptide_forest/knowledge_base.py
@@ -33,5 +33,19 @@ parameters = {
         "prev_score_train",
         "reported_by_",
     },
-    "hyperparameters": {"n_estimators": 100, "max_depth": 22, "max_features": 7},
+    "hyperparameters": {"n_estimators": 100, "max_depth": 22},
+    "ada_hyperparameters": {
+        "n_estimators": 100,
+        "learning_rate": 0.1,
+    },
+    "xgb_hyperparameters": {
+        "n_estimators": 100,
+        "max_depth": 22,
+        "learning_rate": 0.1,
+        "subsample": 1,
+        "colsample_bytree": 1,
+        "gamma": 0,
+        "reg_alpha": 0,
+        "reg_lambda": 1,
+    },
 }
