0c5535b9ea8d6222dd6524260a13b82046751b41
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-05-30 16:06:05 +0200

    use small config json for testing

--- peptide_forest_3.py
+++ peptide_forest_3.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     pf = peptide_forest.PeptideForest(
-        config_path="./docker_test_data/config_local_full.json",  # args.c
+        config_path="./docker_test_data/config_local.json",  # args.c
         output="./docker_test_data/test_output.csv",  # args.o,
         memory_limit=None,  # args.m,
         max_mp_count=None,  # args.mp_limit,
