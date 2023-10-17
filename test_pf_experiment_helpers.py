54300c70ab9c0fe067d31979c06106550578f68f
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-10-17 14:23:42 +0200

    updating tests for get split ootions to also use regex based architecture.

--- test_pf_experiment_helpers.py
+++ test_pf_experiment_helpers.py
@@ -48,42 +48,14 @@ def test_is_matching_filename(filename, value_map, pattern, expected):
     re_pattern = re.compile(pattern)
     assert is_matching_filename(filename, pattern=re_pattern, **value_map) == expected
 
-@pytest.fixture
-def mock_csv_files(tmpdir):
-    sample_data = [
-        (
-            {"raw_data_location": ["something_a_b_c_1.raw"], "other_column": [123]},
-            "test1.csv",
-        ),
-        (
-            {"raw_data_location": ["something_x_y_z_2.raw"], "other_column": [456]},
-            "test2.csv",
-        ),
-        (
-            {"raw_data_location": ["somethingelse_x_y_z_1.raw"], "other_column": [789]},
-            "test3.csv",
-        ),
-    ]
 
-    for data, name in sample_data:
-        df = pd.DataFrame(data)
-        csv_file = tmpdir.join(name)
-        df.to_csv(csv_file, index=False)
+def test_get_split_options():
+    directory = "./test_files"
 
-    return tmpdir
+    pattern = re.compile(r"(?P<group1>[a-zA-Z0-9]+)_?(?P<group2>[a-zA-Z0-9]+)?")
+    result = get_split_options(directory, pattern, col="colA")
 
+    # Assuming you know the expected outcome based on your sample files
+    expected = {"group1": {"value1", "value2"}, "group2": {"valueA", "valueB"}}
 
-def test_get_split_options(mock_csv_files):
-    with patch(
-        "glob.glob",
-        return_value=[
-            str(mock_csv_files.join("test1.csv")),
-            str(mock_csv_files.join("test2.csv")),
-        ],
-    ):
-        options = get_split_options(mock_csv_files)
-
-    assert options["strain"] == {"a", "x"}
-    assert options["fraction"] == {"b", "y"}
-    assert options["enzyme"] == {"c", "z"}
-    assert options["rep"] == {"1", "2"}
+    assert result == expected
