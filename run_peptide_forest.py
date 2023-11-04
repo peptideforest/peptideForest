86295f2f3066d8f9a1b9b678d3faaca0e1f241b6
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-11-04 20:14:58 +0100

    feat: add option to dump prepared dataframes to make training several learning paths faster.

--- run_peptide_forest.py
+++ run_peptide_forest.py
@@ -1,8 +1,12 @@
+import pandas as pd
+
 import peptide_forest
 import multiprocessing as mp
+from pathlib import Path
+import os
 
 
-def run_peptide_forest(config_path, output):
+def run_peptide_forest(config_path, output, buffer_calculated_df=False):
     mp.freeze_support()
     mp.set_start_method("fork", force=True)
 
@@ -10,8 +14,20 @@ def run_peptide_forest(config_path, output):
         config_path=config_path,
         output=output,
     )
-    pf.prep_ursgal_csvs()
-    pf.calc_features()
+
+    config_name = Path(config_path).name
+    buffer_path = "./temp/" + config_name.replace("config_", "temp_df").replace(
+        ".json", ".pkl"
+    )
+
+    if os.path.exists(buffer_path):
+        pf.input_df = pd.read_pickle(buffer_path)
+    else:
+        pf.prep_ursgal_csvs()
+        pf.calc_features()
+        if buffer_calculated_df:
+            pf.input_df.to_pickle(buffer_path)
+
     pf.fit()
     pf.get_results()
     pf.write_output()
