48f3c117edcae7a5233c47970eac31c1ccb8bf0f
tim-hudelmaier <tim.hudelmaier@stud.uni-heidelberg.de>
2023-11-07 17:49:58 +0100

    fix: remove brackets around feature var definition

--- peptide_forest/training.py
+++ peptide_forest/training.py
@@ -350,8 +350,8 @@ def fit_cv(df, score_col, cv_split_data, sensitivity, q_cut, conf):
             model_path=eval_model_path,
         )
         if eval_model_path is None:
-            X = (train_data[features].astype(float),)
-            y = (train_data["is_decoy"].astype(int),)
+            X = train_data[features].astype(float)
+            y = train_data["is_decoy"].astype(int)
             if model_type == "random_forest":
                 rfreg.fit(X=X, y=y)
             elif model_type == "xgboost":
