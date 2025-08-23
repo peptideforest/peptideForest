7003a2afa2951ffefc5cc087ca68d5350d67c34d
Christian Fufezan <christian.x.fufezan@gsk.com>
2025-08-23 16:01:34 +0200

    Fix black and docstring

--- peptide_forest/peptide_forest.py
+++ peptide_forest/peptide_forest.py
@@ -16,17 +16,15 @@ class PeptideForest:
     """Main class to handle peptide forest functionalities."""
 
     def __init__(self, config_path, output):
-        (
-            """Initialize new peptide forest class object.
-        
+        """Initialize new peptide forest class object.
+
         Args:
             config_path (str): a path to a json file with configuration parameters
-            output (str): output file path 
-            initial_engine (str, None): sets initial scoring engine if engine name is given. defaults to None where the
-                                        engine with most PSMs at q-cut is chosen.
+            output (str): output file path
+            initial_engine (str, None): sets initial scoring engine if engine name
+                is  given. defaults to None where the engine with most PSMs
+                at q-cut is chosen.
         """
-            ""
-        )
         # Attributes
         self.output_path = output
         with open(config_path, "r") as json_file:
